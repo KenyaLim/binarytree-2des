e08e46005c41b7c989ad98e6e6ebc59716e255e6
KenyaLim <144202863+KenyaLim@users.noreply.github.com>
2024-12-02 16:29:20 +0700

    Update contoh.py

--- binarytree/contoh.py
+++ binarytree/contoh.py
@@ -1,6 +1,6 @@
 def plus_minus(self):
-    even = []
-    odd = []
+    ganjil = []
+    genap = []
     level_map = {}
     stack = [(self.root, 0)]
 
@@ -13,21 +13,19 @@ def plus_minus(self):
             stack.append((node.right, level + 1))
             stack.append((node.left, level + 1))
 
-    for l in level_map:
-        if l % 2 == 0:
-            for v in level_map[l]:
-                even.append(v)
+    for i in level_map:
+        if i % 2 == 0:
+            for j in level_map[i]:
+                genap.append(j)  # Fix: Even level adds to genap
         else:
-            for v in level_map[l]:
-                odd.append(v)
+            for j in level_map[i]:
+                ganjil.append(j)  # Fix: Odd level adds to ganjil
 
-    total_even = 0
-    for e in even:
-        total_even += e
+    ganjill = 0
+    for e in ganjil:
+        ganjill += e
 
-    total_odd = 0
-    for o in odd:
-        total_odd += o
-
-    result = total_even - total_odd
-    return result
+    genapp = 0
+    for o in genap:
+        genapp += o
+    return genapp - ganjill  # Fix: Subtract ganjil from genap
